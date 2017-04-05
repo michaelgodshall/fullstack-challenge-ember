3373ba26a968699be0874de078ca90b5f9695672
Michael Godshall <michaelgodshall@gmail.com>
2017-04-05 10:09:12 -0700

    Delete a household

--- app/routes/households/show.js
+++ app/routes/households/show.js
@@ -2,21 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    // return this.get('store').findRecord('household', params.household_id, {include: 'persons,vehicles'})
+    return this.get('store').findRecord('household', params.household_id, {include: 'persons,vehicles'})
 
-    const queryParams = { household: params.household_id };
-
-    return Ember.RSVP.hash({
-      household: this.get('store').findRecord('household', params.household_id),
-      persons: this.get('store').query('person', queryParams),
-      vehicles: this.get('store').query('vehicle', queryParams)
-    });
+    // const queryParams = { household: params.household_id };
+    //
+    // return Ember.RSVP.hash({
+    //   household: this.get('store').findRecord('household', params.household_id),
+    //   persons: this.get('store').query('person', queryParams),
+    //   vehicles: this.get('store').query('vehicle', queryParams)
+    // });
   },
 
   actions: {
     delete: function() {
       const controller = this.controller;
-      const household = controller.get('model').household
+      const household = controller.get('model');
       // Delete the household
       household.destroyRecord().then(function() {
         // Redirect to households index
